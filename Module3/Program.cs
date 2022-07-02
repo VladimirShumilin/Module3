373a60a67bdf1d24e5935a91eaee37a8fd28878a
Владимир Шумилин <reg0034@mail.ru>
2022-07-02 13:37:35 +0300

    Задание 3.3.2

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -7,14 +7,18 @@ namespace Module3
         static void Main(string[] args)
         {
             const string MyName = "Jane";
+            const int MyAge = 27;
+            const bool IHavePet = true;
+            const double ShoeSize = 37.5d;
 
             Console.WriteLine(MyName);
 
-            Console.WriteLine("\t  Привет, мир");
-            Console.WriteLine("\t  Мне 27 лет");
-            Console.WriteLine($"\t  My name is \n   {MyName}");
-            Console.WriteLine("\u0040");
-            Console.WriteLine("\x23");
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + IHavePet);
+            Console.WriteLine("My shoe size is " + ShoeSize);
+
+
             Console.ReadKey();
         }
     }
