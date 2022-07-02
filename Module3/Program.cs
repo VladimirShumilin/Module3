2c3008f5eced0551d1e4ac8d3dcb2708d4118d83
Владимир Шумилин <reg0034@mail.ru>
2022-07-02 13:03:05 +0300

    Задание 3.2.5

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -7,7 +7,14 @@ namespace Module3
         static void Main(string[] args)
         {
             const string MyName = "Jane";
+
             Console.WriteLine(MyName);
+
+            Console.WriteLine("Привет, мир");
+            Console.WriteLine("Мне 27 лет");
+            Console.WriteLine("My name is Jane");
+
+            Console.ReadKey();
         }
     }
 }
